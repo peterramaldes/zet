f5afa92bb3f16481c392642e5ef05cf0659d3c5a
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2023-10-29 17:56:52 -0300

    `zet create` command created

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
 pub mod create;
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// The subcommand after `zet` that the program will run
+    #[clap(subcommand)]
+    subcommand: Subcommand,
+}
+
+#[derive(Parser, Debug)]
+enum Subcommand {
+    /// Create the Zettelkasten markdown file and open it using the default $EDITOR or `vi` if
+    /// the env isn´t set.
+    #[clap(visible_alias = "c")]
+    Create,
+}
+
 fn main() -> std::io::Result<()> {
-    create::run()?;
-    Ok(())
+    let cli = Cli::parse();
+
+    return match cli.subcommand {
+        Subcommand::Create => create::run(),
+    };
 }
