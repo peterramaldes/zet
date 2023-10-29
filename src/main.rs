5042fb7d5206ae8751994b88caff8d3ce98c99bb
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2023-10-29 17:35:46 -0300

    Enable `zet` creating the file and open the $EDITOR with it

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
-use std::{env, fs, io};
+use chrono::Utc;
+use std::{
+    env,
+    fs::{self, File},
+    io,
+    os::unix::process::CommandExt,
+    process::Command,
+};
 
 fn main() -> io::Result<()> {
     let path = get_path_from_files();
 
     // Create the path if doesn't exists, thrown an error if cannot
-    fs::create_dir_all(path)?;
+    fs::create_dir_all(&path)?;
+
+    // Create dir if doesn´t exists
+    let today = Utc::now();
+    let folder_name = today.date_naive().format("%Y%m%d");
+    let folder_path = format!("{path}/{folder_name}");
+    let _ = fs::create_dir(&folder_path);
+
+    // Create the file
+    let filename = format!("{}.md", today.format("%Y%m%d%H%M%S"));
+    let file_path = format!("{folder_path}/{filename}");
+    let _ = File::create(&file_path)?; // TODO: can we use this return to open the file?
+
+    // Open the file using the $EDITOR env, using the `vi` if doesn´t set
+    let editor = env::var("EDITOR").unwrap_or_else(|_e| String::from("vi"));
+    Command::new(editor).arg(file_path).exec();
 
     Ok(())
 }
