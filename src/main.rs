4cd9bc46a480979854607fcc5e1dca1edfeefec0
Peter A. Ramaldes <peter.ramaldes@gmail.com>
2023-10-31 20:59:38 -0300

    Create the `zet create` command (#5)
    * Get the path for the zet notes repository
    * Enable `zet` creating the file and open the $EDITOR with it
    * Create `create.rs` file to handle the business logic
    * `zet create` command created
    * Removing unix specific changes to zet be portable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+pub mod create;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// The subcommand after `zet` that the program will run
+    #[clap(subcommand)]
+    subcommand: Subcommand,
+}
+
+#[derive(Parser, Debug)]
+enum Subcommand {
+    /// Create the Zettelkasten markdown file and open it using the default $EDITOR or `vi` if
+    /// the env isn´t set.
+    #[clap(visible_alias = "c")]
+    Create,
+}
+
+fn main() -> std::io::Result<()> {
+    let cli = Cli::parse();
+
+    return match cli.subcommand {
+        Subcommand::Create => create::run(),
+    };
 }
